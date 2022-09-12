4a7736906a824b1ccdd2008e9c8c05243ca82da3
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-12 17:30:35 +0100

    created the UI and added business logic

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.tipcalculator
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,25 +40,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TipTimeScreen(){
-    Column(modifier = Modifier.padding(32.dp),
-    verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        Text(text = stringResource(id = R.string.calculate_tip),
-        fontSize = 24.sp,
-        modifier = Modifier.align(Alignment.CenterHorizontally))
+fun TipTimeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        var amountInput by remember {
+            mutableStateOf("0")
+        }
+        var amount = amountInput.toDoubleOrNull() ?: 0.0
+        val tip = calculateTip(amount)
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumberField()
+        EditNumberField(amountInput) { amountInput = it }
+        Spacer(Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.tip_amount, tip),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
+
 @Composable
-fun EditNumberField() {
-    TextField(value = , onValueChange = )
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = stringResource(id = R.string.cost_of_service)) },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TipCalculatorTheme {
-            TipTimeScreen()
+        TipTimeScreen()
     }
+}
+
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0
+): String {
+    val tip = tipPercent / 100 * amount
+    val formatted_tip = NumberFormat.getCurrencyInstance().format(tip)
+    return formatted_tip
 }
